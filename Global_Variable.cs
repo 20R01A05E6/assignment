5036e86e1d24b837ff0b77d08dde0efca9975478
Purandhar <111522702+20R01A05E6@users.noreply.github.com>
2024-06-21 10:14:58 +0530

    Update Global_Variable.cs

--- Global_Variable.cs
+++ Global_Variable.cs
@@ -1,4 +1,4 @@
-﻿public class Program
+public class Global_Variable
 {
     public static double TotalCost = 0
         ;
@@ -13,15 +13,19 @@
         Console.WriteLine("Removed " + itemName + " from cart. Total Cost: " + TotalCost);
     }
 
+}
+
+public class temp
+{
     public static void Main(string[] args)
     {
-        AddItemToCart("Item-1", 10.99);
-        AddItemToCart("Item-2", 5.99);
-        AddItemToCart("Item-3", 6.49);
-        AddItemToCart("Item-4", 7.36);
-        AddItemToCart("Item-5", 40.22);
-        RemoveItemFromCart("Item 1");
+        Global_Variable.AddItemToCart("Item-1", 10.99);
+        Global_Variable.AddItemToCart("Item-2", 5.99);
+        Global_Variable.AddItemToCart("Item-3", 6.49);
+        Global_Variable.AddItemToCart("Item-4", 7.36);
+        Global_Variable.AddItemToCart("Item-5", 40.22);
+        Global_Variable.RemoveItemFromCart("Item 1");
 
-        Console.WriteLine("Total Cost: " + TotalCost);
+        Console.WriteLine("Total Cost: " + Global_Variable.TotalCost);
     }
 }
